0bf2537821f4d005ca059fa8dc3544646849dfc5
Walter Bonilla <65105034+waltbon@users.noreply.github.com>
2026-03-07 11:39:05 -0600

    feat: GAds added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 
+import Script from "next/script";
 import { Reddit_Sans } from "next/font/google";
 import { cn } from "@/lib/utils";
 
@@ -25,6 +26,23 @@ export default function RootLayout({
           href="https://fonts.gstatic.com"
           crossOrigin="anonymous"
         />
+        {/* Google Analytics */}
+        {process.env.NEXT_PUBLIC_GA_ID && (
+          <>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+              strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+              `}
+            </Script>
+          </>
+        )}
       </head>
       <body className={cn("min-h-screen font-sans antialiased", font.variable)}>
         {children}
